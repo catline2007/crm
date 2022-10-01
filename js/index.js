8d9cc3d3c342a1476d2c332879a43d766d8c13a9
catline2007 <catline2007@yandex.ru>
2022-10-01 21:50:07 +0500

    commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
-product_name = "Сыр Российский"
-count = 2
-price = 200
+let product_name = "Сыр Российский"
+let count = 2
+let price = 200
+let unit = "уп."
+let currency = "руб."
 
 console.log("Наименование: ", product_name)
-console.log("Сумма товаров: ", count * price)
+console.log("Цена: ", price, currency)
+console.log("Количество: ", count, unit)
+console.log("Сумма товаров: ", count * price, currency)
