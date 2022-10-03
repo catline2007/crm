fde616383ae696029b076875ccc2265e432fbf0d
catline2007 <catline2007@yandex.ru>
2022-10-03 09:15:31 +0500

    first commit

---
+++ js/einstein.js
@@ -0,0 +1,5 @@
+const myWeight = 60
+const speedLight = 3e8
+
+console.log(myWeight * Math.pow(speedLight, 2))
+
--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
-let product_name = "Сыр Российский"
-let count = 2
-let price = 200
-let unit = "уп."
+
 let currency = "руб."
 
-console.log("Наименование: ", product_name)
-console.log("Цена: ", price, currency)
-console.log("Количество: ", count, unit)
+const product_name = prompt('Введите наименование товара', '')
+const category = prompt('Введите категорию товара', '')
+const price = Number(prompt('Введите цену товара', ''))
+const unit = prompt('Введите единицу измерения', 'шт.')
+const count = Number(prompt('Введите количество товара', '1'))
+
+
+
+console.log(`Наименование: ${product_name} - это тип: ${typeof product_name}`)
+console.log(`Категория: ${category} - это тип: ${typeof category}`)
+console.log(`Цена: ${price} - это тип: ${typeof price}`)
+console.log(`Количество: ${count} - это тип: ${typeof price}`)
 console.log("Сумма товаров: ", count * price, currency)
+console.log(`На складе ${count} ${unit} товара "${product_name}" на сумму ${count * price} ${currency}`)
+
+
